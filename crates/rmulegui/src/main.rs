fd0b90b4e88caf36f8703b087425cec335dec938
Philip Daniels <Philip.Daniels1971@gmail.com>
2022-12-27 11:05:16 +0000

    Open our first window!

--- crates/rmulegui/src/main.rs
+++ crates/rmulegui/src/main.rs
@@ -2,13 +2,13 @@
 #![forbid(unsafe_code)]
 
 use anyhow::{bail, Result};
+use eframe::egui;
 use rmule::{
     file, get_default_config_dir, initialise_engine, initialise_tokio_tracing,
     inititalise_config_dir,
 };
 use single_instance::SingleInstance;
 use std::path::PathBuf;
-use std::time::Duration;
 use tracing::info;
 
 #[tokio::main]
@@ -20,9 +20,7 @@ async fn main() -> Result<()> {
     inititalise_config_dir(&parsed_args.config_directory, parsed_args.reset_config)?;
     let engine = initialise_engine(&parsed_args.config_directory).await?;
     engine.start().await;
-    loop {
-        std::thread::sleep(Duration::from_secs(5));
-    }
+    start_ui();
     info!("Closing {}", env!("CARGO_PKG_NAME"));
     Ok(())
 }
@@ -99,4 +97,48 @@ fn print_usage() {
 struct ParsedArgs {
     config_directory: PathBuf,
     reset_config: bool,
+}
+
+fn start_ui() {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
